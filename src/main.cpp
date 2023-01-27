7bebb048c7c8977e025a983c591f4b0cbab7cccb
Mustafa Kendigüzel <mustafa.kendiguzel@bil.omu.edu.tr>
2023-01-27 16:56:09 +0300

    add and test temperature

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <WiFiUdp.h>
+#include <DHT.h>
+#include <stdio.h>
 
+
+#define DHTPIN 18 
+#define DHTTYPE DHT11   // DHT 11 
+DHT dht(DHTPIN, DHTTYPE);
 WiFiUDP Udp;
 
 
@@ -13,6 +19,7 @@ unsigned int localPort = 5763; // dinleme por
 
 void setup() {
   Serial.begin(115200);
+  dht.begin();
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
@@ -29,9 +36,18 @@ void setup() {
 }
 
 void loop() {
-  const char* message = "Hello World";
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  const char* message = " HelloWorld Basri";
+  char tempString[28];
+  sprintf(tempString, " Sicaklik: %.2f Nem: %.2f", t,h);
+  const char* temperature = tempString;
   Udp.beginPacket(broadcastIP, localPort);
-  Udp.write((const uint8_t*) message, strlen(message));
+  Udp.write((const uint8_t*) temperature, strlen(temperature));
   Udp.endPacket();
   delay(1000);
 }
